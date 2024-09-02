e73f0173a2431023fb556a387419a9f3c1ae9fdc
CoffeeLexer <eduardasvitkus@icloud.com>
2024-09-02 18:08:20 +0300

    Expand instance

--- Source/main.cpp
+++ Source/main.cpp
@@ -1,7 +1,11 @@
 #include <cstdio>
 #include <cstdint>
 #include <stdexcept>
+#include <vector>
+#include <string>
 #include "vulkan/vulkan.h"
+
+#define GLFW_INCLUDE_NONE
 #include "GLFW/glfw3.h"
 
 class Instance
@@ -21,11 +25,24 @@ public:
 
         uint32_t glfwExtensionCount = 0;
         const char** glfwExtensions;
-
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        std::vector<std::string> extensions;
+        extensions.resize(glfwExtensionCount);
+        for (uint32_t i = 0; i < glfwExtensionCount; i++)
+            extensions.push_back(glfwExtensions[i]);
+
+#if __APPLE__
+        extensions.push_back(VK_KHR_portability_enumeration);
+        const VkInstanceCreateFlags flags = VK_INSTANCE_CREATE_ENUMERATE_PORTABILITY_BIT_KHR;
+#else
+        const VkInstanceCreateFlags flags = 0;
+#endif
 
         VkInstanceCreateInfo ci = {
             .sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
+            .pNext = nullptr,
+            .flags = flags,
             .pApplicationInfo = &info,
             .enabledLayerCount = 0,
             .ppEnabledLayerNames = 0,
@@ -37,6 +54,108 @@ public:
         if (result != VK_SUCCESS)
             throw std::runtime_error("FAILED INSTANCE");
     }
+    ~Instance()
+    {
+        vkDestroyInstance(instance, nullptr);
+    }
+};
+
+class Device
+{
+    VkPhysicalDevice physicalDevice;
+    VkDevice device;
+public:
+    Device()
+    {
+
+    }
+
+    ~Device()
+    {
+
+    }
+};
+
+class Window
+{
+    GLFWwindow *window;
+
+    static void CallbackError(int err, const char* description)
+    {
+        fprintf(stderr, "Error: %s\n", description);
+    }
+
+    static void CallbackKey(GLFWwindow* window, int key, int scancode, int action, int mods)
+    {
+        if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+            glfwSetWindowShouldClose(window, GLFW_TRUE);
+    }
+
+    inline static uint32_t globalInstanceCount = 0;
+    static void GlobalInit()
+    {
+        globalInstanceCount++;
+        if (globalInstanceCount > 1) return;
+
+        if (!glfwInit())
+        {
+            printf("Failed GLFW INIT");
+            return;
+        }
+
+        glfwSetErrorCallback(Window::CallbackError);
+    }
+
+    static void GlobalTerminate()
+    {
+        globalInstanceCount--;
+
+        if (globalInstanceCount > 0) return;
+
+        glfwTerminate();
+    }
+
+public:
+
+    Window()
+    {
+        Window::GlobalInit();
+
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+        glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_TRUE);
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+
+        window = glfwCreateWindow(480, 480, "Concrete Window", nullptr, nullptr);
+
+        if (!window)
+        {
+            fprintf(stderr, "Failed GLFWwindow creation\n");
+            return;
+        }
+
+        glfwSetKeyCallback(window, Window::CallbackKey);
+    }
+    ~Window()
+    {
+        glfwDestroyWindow(window);
+        Window::GlobalTerminate();
+    }
+
+    void SwapBuffers()
+    {
+        //glfwSwapBuffers(window);
+    }
+
+    void PollEvents()
+    {
+        glfwPollEvents();
+    }
+
+    bool IsValid()
+    {
+        return !glfwWindowShouldClose(window);
+    }
 };
 
 class Engine
@@ -46,8 +165,15 @@ public:
 
 };
 
-
 int main()
 {
+    Window window = Window();
+
+    while (window.IsValid())
+    {
+        window.PollEvents();
+        window.SwapBuffers();
+    }
+
     return 0;
 }
